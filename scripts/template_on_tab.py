8dc78b179bb3b0673269db87b05f32aaaddf83b9
Ali Awari <solojungle@users.noreply.github.com>
2023-12-23 20:39:50 -0500

    feat: adding new ui

--- scripts/template_on_tab.py
+++ scripts/template_on_tab.py
@@ -5,21 +5,30 @@ import modules.scripts as scripts
 from modules import script_callbacks
 
 
+def greet(image):
+    return image.rotate(45)
+
+inputs = [
+    gr.Image(label="Input Image", type="pil", show_download_button=True),
+    gr.Image(label="Canvas", interactive=False),
+    gr.ColorPicker(label="Stroke color"),
+    gr.Image(label="Example Style"),
+    gr.Textbox(lines=5, label="Style Description"),
+    gr.Radio(["Use Stroke", "Use Example Style", "Use Description"], label="Sampling Options"),
+    gr.Slider(minimum=1, maximum=10, step=1, label="Number of Samples"),
+]
+
+outputs = [
+    gr.Image(label="Output Image", show_share_button=True),
+]
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=inputs,
+    outputs=outputs,
+)
+
 def on_ui_tabs():
-    with gr.Blocks(analytics_enabled=False) as ui_component:
-        with gr.Row():
-            angle = gr.Slider(
-                minimum=0.0,
-                maximum=360.0,
-                step=1,
-                value=0,
-                label="Angle"
-            )
-            checkbox = gr.Checkbox(
-                False,
-                label="Checkbox"
-            )
-            # TODO: add more UI components (cf. https://gradio.app/docs/#components)
-        return [(ui_component, "Extension Template", "extension_template_tab")]
+        return [(demo, "Extension Template", "extension_template_tab")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
